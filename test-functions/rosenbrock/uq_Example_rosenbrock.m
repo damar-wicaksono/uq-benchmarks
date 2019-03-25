10fcb4583cbfcf55f4546623dffd8b362cde4867
Damar Wicaksono <damar.wicaksono@gmail.com>
2019-03-25 17:06:33 +0100

    Update Rosenbrock function implementation

--- test-functions/rosenbrock/uq_Example_rosenbrock.m
+++ test-functions/rosenbrock/uq_Example_rosenbrock.m
@@ -1,39 +1,24 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% ROSENBROCK FUNCTION - EXAMPLE
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% ROSENBROCK FUNCTION
 %
+% This script contains the model and probabilistic input definitions
+% for a 2-dimensional Rosenbrock's test function in UQLab.
 
-clearvars
+%% 1 - INITIALIZE UQLAB
+%
 uqlab
 
-
-%% INPUT
-% Definition of the input model
+%% 2 - COMPUTATIONAL MODEL
 %
+ModelOpts.Name = 'rosenbrockFunctionModel';
+ModelOpts.mFile = 'uq_rosenbrock';
+myModel = uq_createModel(ModelOpts);
 
-% dimension
-d = 2;
-
-for k = 1:d
-    iopts.Marginals(k).Name = ['X_' num2str(k)];
-    iopts.Marginals(k).Type = 'Uniform';
-    iopts.Marginals(k).Moments = [-5, 10];
+%% 3 - PROBABILISTIC INPUT MODEL
+%
+M = 2;
+for k = 1:M
+    InputOpts.Marginals(k).Type = 'Uniform';
+    InputOpts.Marginals(k).Moments = [-5 10];
 end
 
-clear k
-
-
-myInput = uq_createInput(iopts);
-
-
-%% MODEL
-% Full model
-%
-
-mopts.Name = 'rosenbrockFunctionModel';
-mopts.mFile = 'uq_rosenbrock';
-myModel = uq_createModel(mopts);
-
-
-
-
+myInput = uq_createInput(InputOpts);
--- test-functions/rosenbrock/uq_rosenbrock.m
+++ test-functions/rosenbrock/uq_rosenbrock.m
@@ -1,38 +1,22 @@
-%% *_Rosenbrock Function_*
-%
-% Syntax:
-% Y = UQ_ROSENBROCK(X)
-%
-% The model contains M=d (independent) random variables (X=[X_1,...,X_d]).
-%
-% Input:
-% X     N x M matrix including N samples of M stochastic parameters
-%
-% Output/Return:
-% Y     column vector of length N including evaluations using rosenbrock
-%       function
-%
-
-
-%%%
 function Y = uq_rosenbrock(X)
-
-
-%% Check
+% UQ_ROSENBROCK computes the M-dimensional Rosenbrock's function.
 %
+%   The M-dimensional Rosenbrock's function is defined as:
+%   $$f(\mathbf{x}) = \sum_{m=1}^{M-1} [100(x_{m+1} - x_m^2)^2 + (x_m - 1)^2]$$
+%
+%   Y = UQ_ROSENBROCK(X) computes the Rosenbrock's function for N-by-M 
+%   input matrix X, where N and M are the number of evaluation points and
+%   the number of dimensions, respectively.
+%
+
+%% Check input
 narginchk(1,1)
 
+%% Get the number of dimensions
+M = size(X,2);
 
-%% Evaluation
-%
-% $$f(\mathbf{x}) = \sum_{i=1}^{d-1} [100(x_{i+1} - x_i^2)^2 + (x_i-1)^2]$$
-%
-
-% dimension
-d = size(X,2);
-
-index = 1:(d-1);
-Y = sum((100*(X(:,index+1)-X(:,index).^2).^2 + (X(:,index)-1).^2),2);
-
+%% Evaluate function
+idx = 1:(M-1);
+Y = sum((100*(X(:,idx+1)-X(:,idx).^2).^2 + (X(:,idx)-1).^2),2);
 
 end
