3947c880a5af5e5dbeacdd33fb3a94131e9ea4bd
Damar Wicaksono <damar.wicaksono@gmail.com>
2019-03-25 22:42:00 +0100

    Update the exp-tanh function

--- test-functions/expTanh/uq_exp_tanh.m
+++ test-functions/expTanh/uq_exp_tanh.m
@@ -1,36 +1,20 @@
-%% *_Exp-Tanh Function_*
+function Y = uq_expTanh(X)
+% UQ_EXPTANH computes the 2-dimensional exp-tanh test function.
 %
-% Syntax:
-% Y = UQ_EXP_TANH(X)
+%   The 2-dimensional exp-tanh function is defined as:
+%   $$f(\mathbf{x}) = exp(-x_1) tanh(5x_2)$$
 %
-% The model contains M=2 (independent) random variables (X=[X_1,X_2]).
-%
-% Input:
-% X     N x M matrix including N samples of M stochastic parameters
-%
-% Output/Return:
-% Y     column vector of length N including evaluations using exp_tanh
-%       function
-%
+%   Y = UQ_EXPTANH(X) computes the exp-tanh function for N-by-2 input
+%   matrix X, where N is the number of evaluation points; and returns
+%   column vector of length N.
 
-
-%%%
-function Y = uq_exp_tanh(X)
-
-
-%% Check
+%% Check input
 %
 narginchk(1,1)
+assert(size(X,2)==2,'Only 2 input variables are allowed!')
 
-assert(size(X,2)==2,'only 2 input variables allowed')
-
-
-%% Evaluation
+%% Evaluate function
 %
-% $$f(\mathbf{x}) = exp(-x_1) tanh(5x_2)$$
-%
-
-Y = exp(-X(:,1)).*tanh(5*X(:,2));
-
+Y = exp(-X(:,1)) .* tanh(5*X(:,2));
 
 end
